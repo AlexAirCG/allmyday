709b66978d86c10fc5489cb8888bdc54706d1df1
AlexAirCG <alexairggg@yandex.ru>
2024-06-08 10:50:11 +0300

    add tests playwright add git hab actions

---
+++ tests/courses-list.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from "@playwright/test";
+
+test("create delete", async ({ page }) => {
+  await page.goto("/");
+  await page.getByRole("button", { name: "Удалить" }).click();
+  await page.getByPlaceholder("название").click();
+  await page.getByPlaceholder("название").fill("Test course");
+  await page.getByPlaceholder("описание").click();
+  await page.getByPlaceholder("описание").fill("description");
+  await page.getByRole("button", { name: "Добавить" }).click();
+});
