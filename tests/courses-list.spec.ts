0b354f71d5aac351b24ff21c5055863b0dec6446
AlexAirCG <alexairggg@yandex.ru>
2024-06-12 09:40:20 +0300

    fix tests courses-list.spec

--- tests/courses-list.spec.ts
+++ tests/courses-list.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test("create delete", async ({ page }) => {
-  await page.goto("/");
+  // await page.goto("/");
   await page.getByRole("button", { name: "Удалить" }).click();
   await page.getByPlaceholder("название").click();
   await page.getByPlaceholder("название").fill("Test course");
